27cd268c208ab3851df0bb33e9de8cbb9e9f6645
logarythm <mahmood.ihsan98989@gmail.com>
2023-01-31 13:38:21 +0000

    added code to change background color of text area depending on time

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,35 @@
 var today = moment();
 
 $("#currentDay").text(today.format("llll"));
+
+// code to change background of text area
+
+
+
+var currenthour = moment().hour()
+
+console.log(currenthour)
+
+$(".time-block").each(function () {
+    var blocktime = parseInt($(this).attr("id"));
+
+    if (blocktime < currenthour){
+        $(this).children(".description").removeClass("future");
+        $(this).children(".description").removeClass("present");
+        $(this).children(".description").addClass("past");
+    }
+    else if ( blocktime === currenthour){
+        $(this).children(".description").removeClass("future");
+        $(this).children(".description").addClass("present");
+        $(this).children(".description").removeClass("past");
+    }
+    else {
+        $(this).children(".description").Class("future");
+        $(this).children(".description").removeClass("present");
+        $(this).children(".description").removeClass("past");
+    }
+    
+    
+})
+
+
